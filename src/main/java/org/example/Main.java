5b85db4b92776cbd22aac9e35d68a3548fefec0c
muhammad.khadafi <yusuf.khadafi@pusilkom.com>
2024-02-08 11:40:12 +0700

    refactoring code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -25,20 +25,19 @@ public class Main {
     }
 
     private static BigInteger factorial(int n) {
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+        BigInteger result = BigInteger.ONE;
+        for (int i = 2; i <= n; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
         }
-        return (n <= 1) ? BigInteger.ONE : BigInteger.valueOf(n).multiply(factorial(n - 1));
+        return result;
     }
 
     private static String generateLargeString(int size) {
-        String result = "";
+        StringBuilder builder = new StringBuilder(size);
         for (int i = 0; i < size; i++) {
-            result += "a";
+            builder.append("a");
         }
-        return result;
+        return builder.toString();
     }
 
 }
